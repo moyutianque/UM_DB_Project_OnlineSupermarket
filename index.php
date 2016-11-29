16f03a83145c836aafc2ffae383ca49978f4a5ef
WANG ZEHAO <moyutianque@outlook.com>
2016-11-29 23:07:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Template</title>
+
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style-xlarge.css" rel="stylesheet" type="text/css"/>
+
+
+    </head>
+    <body class="landing">
+
+		<!-- Banner -->
+			<section id="banner">
+				<p>WE ARE HERE WAITING FOR YOU</p>
+                <h2>FAMER'S SUPERMARKET</h2>
+
+				<ul class="actions">
+					<li>
+						<a href="../authentication/login.php" class="button big">Login</a>
+					</li>
+                    <li>
+                        <a href="../authentication/register.php" class="button big">Signup</a>
+                    </li>
+				</ul>
+
+			</section>
+
+	</body>
+</html>
